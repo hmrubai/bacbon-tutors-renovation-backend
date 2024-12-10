4d4b831532e2e881bbc2527397e78893ca5b1c38
hmrubai <rubai.mobarak@gmail.com>
2024-12-10 11:57:47 +0600

    Project Initialized

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\SubMenuController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BrandController;
+use Illuminate\Support\Facades\Route;
+
+
+// Protected Routes
+Route::group(['middleware' => ['auth:api',]], function () {
+    Route::group(['middleware' => ['role:system-admin,super-admin,admin']], function () {
+        Route::apiResource('users', UserController::class);
+        Route::apiResource('menus', MenuController::class);
+        Route::apiResource('sub-menus', SubMenuController::class);
+    });
+    Route::apiResource('categories', CategoryController::class);
+
+
+
+
+});
